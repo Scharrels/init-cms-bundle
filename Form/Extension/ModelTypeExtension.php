8acdfa00f66f19c69186bd1f905ea2c1636eda8a
Lennard Klein <bata@lennard.org>
2017-12-03 23:07:39 +0100

    fix getExtendedType deprecation

--- Form/Extension/ModelTypeExtension.php
+++ Form/Extension/ModelTypeExtension.php
@@ -59,9 +59,7 @@ class ModelTypeExtension extends AbstractTypeExtension {
 
 
     /**
-     * Returns the name of the type being extended.
-     *
-     * @return string The name of the type being extended
+     * {@inheritdoc}
      */
     public function getExtendedType()
     {
